674daec8ea04f9add73bc2a89212c9268db4c829
hafidluqman50 <hafidluqmanulhakim@gmail.com>
2021-04-07 13:16:49 +0800

    Fix Struk Admin Kasir

--- resources/views/Kasir/transaksi/struk.blade.php
+++ resources/views/Kasir/transaksi/struk.blade.php
@@ -9,12 +9,9 @@
 	<style>
 		@page {
 			margin:0!important;
-			size:5cm 7cm;
-			size:portrait;
-  			-webkit-print-color-adjust: exact !important;
-		}
-		body {
-			background-color:grey;
+			/*size:50mm 70mm;*/
+			/*size:portrait;*/
+  			-webkit-print-color-adjust:exact!important;
 		}
 		.struk {
 			width:100%;
@@ -23,14 +20,17 @@
 			padding-left:1%;
 			padding-right:1%;
 			position:relative;
-  			box-shadow: 5px 10px #888888;
+			border:1px solid black;
+			box-shadow: -1px 0px 8px 2px rgba(196,196,196,0.89);
+			-webkit-box-shadow: -1px 0px 8px 2px rgba(196,196,196,0.89);
+			-moz-box-shadow: -1px 0px 8px 2px rgba(196,196,196,0.89);
 		}
 		.layout-struk {
-			width:100%;
-			min-height:100%;
-			position:absolute;
-			top:0;
-			background-color: lightgrey;
+			width:57mm;
+			margin:0 auto;
+			/*position:absolute;*/
+			/*top:0;*/
+			/*left:40%;*/
 		}
 		.struk-header {
 			width:100%;
@@ -40,7 +40,7 @@
 		}
 		.struk-info {
 			width:100%;
-			height:90px;
+			height:60px;
 			margin-top:5px;
 			margin-bottom:5px;
 			border-bottom:1px solid lightgrey;
@@ -52,7 +52,7 @@
 		}
 		.struk-total {
 			width:100%;
-			height:100px;
+			min-height:100px;
 			border-top:1px solid lightgrey;
 			border-bottom:1px solid lightgrey;
 			margin:0;
@@ -71,18 +71,18 @@
 			<div class="struk">
 				<div class="struk-header">
 					{{-- <div class="row align-items-center justify-content-center"> --}}
-						<h5 style="margin-bottom:7px;"><b>JupiterPOS</b></h5>
-						<p style="margin:0;">{{ $info_outlet->nama_outlet }}</p>
-						<p style="margin:0;">{{ $info_outlet->alamat_outlet }}</p>
-						<p><span class="fas fa-phone"></span> {{ $info_outlet->nomor_telepon }}</p>
+						<h5 style="margin-bottom:7px; font-size:17px;"><b>JupiterPOS</b></h5>
+						<p style="margin:0; font-size:15px;">{{ $info_outlet->nama_outlet }}</p>
+						<p style="margin:0; font-size:15px;">{{ $info_outlet->alamat_outlet }}</p>
+						<p style="font-size:15px;"><span class="fas fa-phone"></span> {{ $info_outlet->nomor_telepon }}</p>
 					{{-- </div> --}}
 				</div>
 				<div class="struk-info">
 					@php
 						$jam = explode(' ',$transaksi_info->created_at)
 					@endphp
-					<p>{{ human_date($transaksi_info->tanggal_transaksi).' '.$jam[1] }}</p>
-					<p>Kasir : {{ $transaksi_info->name }}</p>
+					<p style="font-size:13px;">{{ human_date($transaksi_info->tanggal_transaksi).' '.$jam[1] }}</p>
+					<p style="font-size:13px;">Kasir : {{ $transaksi_info->name }}</p>
 				</div>
 				<div class="struk-menu">
 					<table width="100%">
@@ -91,17 +91,17 @@
 							$explode = explode(':',$element->varian);
 						@endphp
 						<tr>
-							<td><b>{{ $element->nama_item }}</b>
-								<td><b><span style="float:right">{{ format_rupiah($element->sub_total) }}</span></b></td>
+							<td style="font-size:13px;"><b>{{ $element->nama_item }} : </b>
+								<td style="font-size:13px;"><b><span style="float:right">{{ format_rupiah($element->sub_total) }}</span></b></td>
 								@if ($element->varian != null)
 								<tr>
-									<td>-----{{$explode[0]}} : </td>
+									<td style="font-size:13px;">&nbsp;&nbsp;{{$explode[0]}} : </td>
 									<td><span style="float:right">{{ format_rupiah($explode[1]) }}</span></td>
 								</tr>
 								@endif
 								@if ($element->keterangan != null)
 								<tr>
-									<td>-----{{$element->keterangan}}</td>
+									<td style="font-size:13px;">&nbsp;&nbsp;{{$element->keterangan}}</td>
 								</tr>
 								@endif
 							</td>
@@ -112,21 +112,21 @@
 				<div class="struk-total">
 					<table width="100%">
 						<tr>
-							<td><b>-----------Total Harga------------ : </b></td>
-							<td><span style="float:right;"><b>{{ format_rupiah($transaksi_info->total_harga) }}</b></span></td>
+							<td style="font-size:13px;"><b>Total Harga : </b></td>
+							<td style="font-size:13px;"><span style="float:right;"><b>{{ format_rupiah($transaksi_info->total_harga) }}</b></span></td>
 						</tr>
 						<tr>
-							<td><b>-----------Total Bayar------------ : </b></td>
-							<td><span style="float:right;"><b>{{ format_rupiah($transaksi_info->total_bayar) }}</b></span></td>
+							<td style="font-size:13px;"><b>Total Bayar : </b></td>
+							<td style="font-size:13px;"><span style="float:right;"><b>{{ format_rupiah($transaksi_info->total_bayar) }}</b></span></td>
 						</tr>
 						<tr>
-							<td><b>-----------Kembalian------------- :</b></td>
-							<td><span style="float:right;"><b>{{ format_rupiah($transaksi_info->kembalian) }}</b></span></td>
+							<td style="font-size:13px;"><b>Kembalian :</b></td>
+							<td style="font-size:13px;"><span style="float:right;"><b>{{ format_rupiah($transaksi_info->kembalian) }}</b></span></td>
 						</tr>
 					</table>
 				</div>
 				<div class="struk-catatan">
-					<p>{{ $info_outlet->catatan }}</p>
+					<p style="font-size:13px;">{{ $info_outlet->catatan }}</p>
 				</div>
 			</div>
 		</div>
